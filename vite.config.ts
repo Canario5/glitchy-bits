0eb3f714a2533b519505cbb886632cb0d2ae6e05
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-07-25 17:04:59 +0200

    build(vite): add strictPort option to vite config
    - `strictPort: true` ensures that Vite will not automatically increment the port number if default port (5173) is in use, and will instead throw an error.

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    strictPort: true,
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 });
